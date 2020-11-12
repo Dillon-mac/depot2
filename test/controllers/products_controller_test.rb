f9848aa1d95551225371c476ced21d21c12eab2b
Dillon_Mac <dillonm93@gmail.com>
2020-11-12 01:23:44 +0000

    added cart and line item models.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,7 +17,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: "New Book" } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -45,4 +45,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "can't delete product in cart" do
+  assert_difference('Product.count', 0) do
+    delete product_url(products(:two))
+  end
+
+  assert_redirected_to products_url
+end
+
 end
