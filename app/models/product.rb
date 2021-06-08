a0584d64212f5c6033a8eb8ad08c8d9be8375aa8
Dillon-mac <56975667+Dillon-mac@users.noreply.github.com>
2021-06-08 20:57:29 +0100

    added checkout button

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ApplicationRecord
-  before_destroy :ensure_not_referenced_by_any_line_item
-  has_many :line_items
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :image_url, allow_blank: true, format: {
   with: %r{\.(gif|jpg|png|jpeg)\Z}i,
   message: 'must be a URL for GIF, JPG, JPEG or PNG image.'
-}
+  }
+  before_destroy :ensure_not_referenced_by_any_line_item
+  has_many :line_items
 end
 
 private
